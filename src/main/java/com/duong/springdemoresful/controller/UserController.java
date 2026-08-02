d8e47bc90addfa4921ab1a9b1e9896428834f1a4
TungDuong-Dev <devopptungduong@gmail.com>
2026-08-02 22:24:10 +0700

    Validation

--- src/main/java/com/duong/springdemoresful/controller/UserController.java
+++ src/main/java/com/duong/springdemoresful/controller/UserController.java
@@ -4,6 +4,7 @@ package com.duong.springdemoresful.controller;
 import com.duong.springdemoresful.helper.ApiResponse;
 import com.duong.springdemoresful.model.User;
 import com.duong.springdemoresful.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +23,7 @@ public class UserController {
 
 
 	@PostMapping("/users")
-	public ResponseEntity<ApiResponse<User>>createUser(@RequestBody User user){
+	public ResponseEntity<ApiResponse<User>>createUser(@Valid @RequestBody User user){
 		User createUser = userService.createUser(user);
 		return ApiResponse.created(createUser);
 	}
@@ -43,7 +44,7 @@ public class UserController {
 	}
 
 	@PutMapping ("/users/{id}")
-	public ResponseEntity<ApiResponse<User>>updateUserById(@PathVariable int id, @RequestBody User inputUser){
+	public ResponseEntity<ApiResponse<User>>updateUserById(@PathVariable int id, @Valid @RequestBody User inputUser){
 		inputUser.setId(id);
 		User updateUser = userService.updateUser(inputUser);
 
--- src/main/java/com/duong/springdemoresful/helper/GlobalExceptionHandle.java
+++ src/main/java/com/duong/springdemoresful/helper/GlobalExceptionHandle.java
@@ -3,8 +3,13 @@ package com.duong.springdemoresful.helper;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandle {
@@ -12,4 +17,19 @@ public class GlobalExceptionHandle {
     public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex){
         return ApiResponse.error(HttpStatus.NOT_FOUND,ex.getMessage());
     }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handleMisMatched(MethodArgumentTypeMismatchException ex){
+        return  ApiResponse.error(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errorList = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+        String errors = String.join("; ", errorList);
+
+        ApiResponse<Object> response = new ApiResponse<>(HttpStatus.BAD_REQUEST, errors,  errorList, "VALIDATION_ERROR");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
 }

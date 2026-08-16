3475e8782fa4b985fbdabaec65da8d1180d98cc3
TungDuongDevopp <xaydaa8@gmail.com>
2026-08-16 16:12:11 +0700

    download file

--- src/main/java/com/duong/springdemoresful/controller/FileController.java
+++ src/main/java/com/duong/springdemoresful/controller/FileController.java
@@ -5,7 +5,12 @@ import com.duong.springdemoresful.helper.ApiResponse;
 import com.duong.springdemoresful.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +33,16 @@ public class FileController {
         fileService.createUploadFolder(baseURI + folder);
        String uploadedFile =  fileService.store(file,folder);
         return ApiResponse.success(new FileResponse(uploadedFile, Instant.now()),"success");
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<Resource> downLoad(@RequestParam("fileName") String fileName,
+                                             @RequestParam("folder") String folder) throws URISyntaxException, IOException {
+        InputStreamResource resource = fileService.getFile(fileName,folder);
+        return  ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"%s\"".formatted(fileName))
+                .contentLength(fileService.getFileLength(fileName,folder))
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 }
--- src/main/java/com/duong/springdemoresful/service/FileService.java
+++ src/main/java/com/duong/springdemoresful/service/FileService.java
@@ -3,10 +3,12 @@ package com.duong.springdemoresful.service;
 import com.duong.springdemoresful.helper.exception.InvalidTypeFileException;
 import com.duong.springdemoresful.helper.exception.StorageException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -49,6 +51,7 @@ public class FileService {
             System.out.println(">>> SKIP MAKING DIRECTORY, ALREADY EXISTS");
         }
     }
+
     public String store(MultipartFile file, String folder) throws URISyntaxException, IOException {
 
         String fileName = file.getOriginalFilename();
@@ -80,6 +83,26 @@ public class FileService {
                     StandardCopyOption.REPLACE_EXISTING);
         }
         return finalName;
+    }
+
+    public long getFileLength(String fileName,String folder) throws URISyntaxException {
+       URI uri = new URI("%s%s/%s".formatted(baseURI, folder, fileName));
+       Path path = Paths.get(uri);
+       File tmpDir = new File(path.toString());
+       if(tmpDir.isDirectory()||!tmpDir.exists()) {
+           throw new StorageException("File is not exist, please upload again!");
+       }
+       return tmpDir.length();
+    }
+
+    public InputStreamResource getFile(String fileName, String folder) throws URISyntaxException, IOException {
+        URI uri = new URI("%s%s/%s".formatted(baseURI, folder, fileName));
+        Path path = Paths.get(uri);
+        File file = new File(path.toString());
+        if (!file.exists() || file.isDirectory()) {
+            throw new StorageException("File '%s' does not exist in folder '%s'!".formatted(fileName, folder));
+        }
+        return new InputStreamResource(new FileInputStream(file));
     }
 
 
